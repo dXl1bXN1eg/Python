c27b3118fa841c441feaa331a00ef05905f58bb1
tsgk-coder <cydepo@gmail.com>
2024-10-07 10:27:21 +0300

    Update main_1.py

--- main_1.py
+++ main_1.py
@@ -1,1 +1,13 @@
+sayi = 3
+ad = "Cemal"
+k = true
+s1 = 'E'
 
+
++    ---> toplama
+-    ---> çıkarma
+*    ---> çarpma  
+/    ---> bölme
+//   ---> tamsayı bölme
+%    ---> mod
+**   ---> kuvvet alma
