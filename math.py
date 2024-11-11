1c943cb58c413333db88e71858f3084714d30918
tsgk-coder <cydepo@gmail.com>
2024-11-11 11:37:45 +0300

    Create math.py

---
+++ math.py
@@ -0,0 +1,72 @@
+# Matematik formülü
+
+import math
+
+#s = math.pow() küp alma
+#d = math.ceil(3)
+#f = math.sin(2)
+#g = math.cos(3)
+#h = math.tan(3)
+#j = math.pi(2)
+#f = math.factorial(5)
+#f = math.sqrt(5) kök alma
+#print(f)
+
+"""
+toplam = 0
+for i in range(1,11):
+    a = i
+    b = math.factorial(i)
+    toplam  = toplam + (a/b)
+
+#print(int(toplam))
+"""
+
+"""
+total = 0
+for i in range(1,51):
+    total = total + math.sqrt(i)
+
+print(total)
+"""
+
+"""
+x = int(input("sayi gir= "))
+sonuc =  0
+if x < 5:
+    sonuc = -2*x + 3
+else:
+    sonuc = math.pow(x,3)*math.pi
+
+print(sonuc)
+"""
+
+"""
+alfa = int(input("Alfa değeri giriniz: "))
+beta = int(input("Beta değeri giriniz: "))
+total = alfa + beta
+cikarim = alfa - beta
+sonuc = 2*math.sin((total / 2))*math.cos(cikarim/2)
+print(sonuc)
+"""
+
+"""
+a = 3
+b = 4
+a = math.pow(a,2)
+b = math.pow(b,2)
+total = a + b
+s = math.pi * math.sqrt(total)
+print(s)
+"""
+
+"""x = 5
+y = 9
+
+f = (math.pi * x) / 2
+t = math.sqrt(x + math.pow(y,3)) / math.sin(y)
+toplam = f + t
+print(toplam)
+"""
+
+
