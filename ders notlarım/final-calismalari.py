3adabf64de7f3edf30d2b5b29994f7b6f40111ca
tsgk-coder <cydepo@gmail.com>
2024-12-10 01:52:32 -0500

    Create final-calismalari.py

---
+++ ders notlarım/final-calismalari.py
@@ -0,0 +1,20 @@
+# fonk = lambda param1=9: param1 % 2 == 0
+# x = range(10)
+# c = list(map(fonk,x))
+# print(c)
+
+# filtered_liste_2 = list(filter(lambda x: (x % 2 == 0), list_number))
+# print(filtered_liste_2)
+
+# type()
+# orn1.replace("i","*")
+# len()
+# isdigit()
+# count("a")
+
+# open
+# close
+
+# r read
+# w write
+# a append
